9d0ddf3530411fc10eff2bbb09f93cc5dc00a2cb
arifm09 <86922587+arifm09@users.noreply.github.com>
2024-07-31 21:04:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+let codes = document.querySelectorAll(".code")
+// change, input
+codes.forEach((code,index)=>{
+    
+    code.addEventListener("input",()=>{
+        if(code.value.length == 1){
+             if(index < codes.length-1){
+                   codes[index+1].focus()
+                   codes[index+1].classList.add("focused")
+             }
+        }
+    })
+    // keyup, keydown(press)
+    code.addEventListener("keydown",(e)=>{
+        if(e.key == "Backspace"){
+              if(index > 0 && code.value.length == 0){
+                
+                codes[index-1].focus()
+                codes[index-1].classList.add("focused")
+              }
+        }
+    })
+    code.addEventListener("blur", ()=>{
+          code.classList.remove("focused")
+    })
+})

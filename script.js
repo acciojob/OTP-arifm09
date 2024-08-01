4528374ad713f94cd89feb20ce28d33903d8e61b
arifm09 <86922587+arifm09@users.noreply.github.com>
2024-08-01 15:08:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,22 +3,23 @@ let codes = document.querySelectorAll(".code");
 
 
 codes.forEach( (code,index) =>{
-code.addEventListener("input", () => {
-    if (index < 5 && code.value.length == 1) {
-      codes[index + 1].focus();
-      codes[index + 1].classList.add("focused");
-      code.classList.remove("focused");
-    }
-  });
-    code.addEventListener("keydown", (e)=>{
-        if(e.key == "Backspace" && index>0 && code.value.length==0){
-            codes[index-1].focus();
-            codes[index-1].classList.add("focused");
-			  code.classList.remove("focused");
+ code.addEventListener('input', () => {
+        if (index < 5 && code.value.length === 1) {
+            codes[index + 1].focus();
         }
-    })
+    });
 
-    code.addEventListener("blur",()=>{
-        code.classList.remove("focused");
-    })
+    code.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && index > 0 && code.value.length === 0) {
+            codes[index - 1].focus();
+        }
+    });
+
+    code.addEventListener('focus', () => {
+        code.classList.add('focused');
+    });
+
+    code.addEventListener('blur', () => {
+        code.classList.remove('focused');
+    });
 })

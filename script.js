e225b614548500ad20f751e76cfa1bad3014a0c0
arifm09 <86922587+arifm09@users.noreply.github.com>
2024-08-01 15:06:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,13 @@ let codes = document.querySelectorAll(".code");
 
 
 codes.forEach( (code,index) =>{
-
-    code.addEventListener("input", ()=>{
-       if(index<5 && code.value.length==1){
-          codes[index+1].focus();
-          codes[index+1].classList.add("focused");
-		   code.classList.remove("focused");
-       }
-    })
-
+code.addEventListener("input", () => {
+    if (index < 5 && code.value.length == 1) {
+      codes[index + 1].focus();
+      codes[index + 1].classList.add("focused");
+      code.classList.remove("focused");
+    }
+  });
     code.addEventListener("keydown", (e)=>{
         if(e.key == "Backspace" && index>0 && code.value.length==0){
             codes[index-1].focus();
